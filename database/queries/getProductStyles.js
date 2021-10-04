7fa8d1481031cf940767b0687d6b1bf741dbe06c
thechosenginger <sewell.matthewm@gmail.com>
2021-10-04 15:00:45 -0500

    forgot to save file

--- database/queries/getProductStyles.js
+++ database/queries/getProductStyles.js
@@ -3,11 +3,11 @@ const { pool } = require('../index');
 const getProductStyles = (productId, callback) => {
   const queryString = `SELECT * FROM styles WHERE ${productId};`;
 
-  pool.query(queryString, (err, relatedData) => {
+  pool.query(queryString, (err, stylesData) => {
     if (err) {
       callback(err, null);
     } else {
-      callback(null, relatedData);
+      callback(null, stylesData);
     }
   });
 };
