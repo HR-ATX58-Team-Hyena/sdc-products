8361ce6b392119a17d07567b93bbe5e345575930
thechosenginger <sewell.matthewm@gmail.com>
2021-10-07 15:49:14 -0500

    commit before deployment

--- database/queries/getProductStyles.js
+++ database/queries/getProductStyles.js
@@ -2,6 +2,7 @@ const { pool } = require('../index');
 
 const getProductStyles = (productId, callback) => {
   const queryString = `SELECT * FROM styles WHERE product_id = ${productId};`;
+  // const queryString = `SELECT product_id FROM styles SELECT json_agg(styles) FROM`
   pool.query(queryString, (err, stylesData) => {
     if (err) {
       callback(err, null);
